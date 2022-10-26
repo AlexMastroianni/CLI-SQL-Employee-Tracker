2a2b95ea4840277f5027cc76099cf2630acdee8f
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-10-26 11:02:06 +1100

    added hardcode seeds for demo and testing

---
+++ db/seeds.sql
@@ -0,0 +1,43 @@
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+INSERT INTO department (name)
+VALUES ("Marketing");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Executive", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Tech Lead", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 350000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Head of Marketing", 150000, 5);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alex", "Mastroianni", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lisa", "Smith", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("James", "Betts", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Jay", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Peter", "Parker", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Karyn", "Miller", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Zoey", "Hooper", 1, 2);
